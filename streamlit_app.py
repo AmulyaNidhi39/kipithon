2ee8385a7875631a0ca6da53e8596f2be65f4fe8
AmulyaNidhi39 <84920720+AmulyaNidhi39@users.noreply.github.com>
2022-12-21 23:40:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,37 +6,15 @@ import tableauserverclient as TSC
 
 # Set up connection.
 tableau_auth = TSC.PersonalAccessTokenAuth(
-    st.secrets["tableau"]["token_name"],
-    st.secrets["tableau"]["personal_access_token"],
-    st.secrets["tableau"]["site_id"],
+    st.secrets["tableau"]["MyToken"],
+    st.secrets["tableau"]["ZpGk5qMJQIytxtHerljfTg==:22aDQjNYtWww9McC5pabyXY1qqGwkcs7"],
+    st.secrets["tableau"]["site21"],
 )
-server = TSC.Server(st.secrets["tableau"]["server_url"], use_server_version=True)
+server = TSC.Server(st.secrets["tableau"]["https://prod-useast-a.online.tableau.com/"], use_server_version=True)
 
 
-# Get various data.
-# Explore the tableauserverclient library for more options.
-# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
-@st.experimental_memo(ttl=600)
-def run_query():
-    with server.auth.sign_in(tableau_auth):
-
-        # Get all workbooks.
-        workbooks, pagination_item = server.workbooks.get()
-        workbooks_names = [w.name for w in workbooks]
-
-        # Get views for first workbook.
-        server.workbooks.populate_views(workbooks[0])
-        views_names = [v.name for v in workbooks[0].views]
-
-        # Get image & CSV for first view of first workbook.
-        view_item = workbooks[0].views[0]
-        server.views.populate_image(view_item)
-        server.views.populate_csv(view_item)
-        view_name = view_item.name
-        view_image = view_item.image
-        # `view_item.csv` is a list of binary objects, convert to str.
-        view_csv = b"".join(view_item.csv).decode("utf-8")
-
-        return workbooks_names, views_names, view_name, view_image, view_csv
-
-workbooks_names, views_names, view_name, view_image, view_csv = run_query()
+[tableau]
+token_name="MyToken"
+personal_access_token = "ZpGk5qMJQIytxtHerljfTg==:22aDQjNYtWww9McC5pabyXY1qqGwkcs7"
+server_url = "https://prod-useast-a.online.tableau.com/"
+site_id = "site21"
