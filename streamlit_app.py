414a4d9ceef25333d52e2826f29f155fbf206918
AmulyaNidhi39 <84920720+AmulyaNidhi39@users.noreply.github.com>
2022-12-21 23:40:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,10 +11,3 @@ tableau_auth = TSC.PersonalAccessTokenAuth(
     st.secrets["tableau"]["site21"],
 )
 server = TSC.Server(st.secrets["tableau"]["https://prod-useast-a.online.tableau.com/"], use_server_version=True)
-
-
-[tableau]
-token_name="MyToken"
-personal_access_token = "ZpGk5qMJQIytxtHerljfTg==:22aDQjNYtWww9McC5pabyXY1qqGwkcs7"
-server_url = "https://prod-useast-a.online.tableau.com/"
-site_id = "site21"
