d04f10462e21f56d9d88c58f698fa78e5760a17f
AmulyaNidhi39 <84920720+AmulyaNidhi39@users.noreply.github.com>
2022-12-21 21:00:56 +0530

    app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+# streamlit_app.py
+
+import streamlit as st
+import tableauserverclient as TSC
+
+
+# Set up connection.
+tableau_auth = TSC.PersonalAccessTokenAuth(
+    st.secrets["tableau"]["token_name"],
+    st.secrets["tableau"]["personal_access_token"],
+    st.secrets["tableau"]["site_id"],
+)
+server = TSC.Server(st.secrets["tableau"]["server_url"], use_server_version=True)
+
+
+# Get various data.
+# Explore the tableauserverclient library for more options.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def run_query():
+    with server.auth.sign_in(tableau_auth):
+
+        # Get all workbooks.
+        workbooks, pagination_item = server.workbooks.get()
+        workbooks_names = [w.name for w in workbooks]
