04363386076aa8440c577f6eae3d2fcfa04cf257
AmulyaNidhi39 <84920720+AmulyaNidhi39@users.noreply.github.com>
2022-12-22 12:59:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,8 @@ server_url = 'https://prod-useast-a.online.tableau.com'
 user = 'amulya.s.nidhi@kipi.bi'
 password = 'Kipithon@123'
 site = 'Site21'
-tableau_auth = TSC.TableauAuth(username=user, password=password, site_id=site)server = TSC.Server(server_url, use_server_version=True)
+tableau_auth = TSC.TableauAuth(username=user, password=password, site_id=site)
+server = TSC.Server(server_url, use_server_version=True)
 
 # Get various data.
 # Explore the tableauserverclient library for more options.
