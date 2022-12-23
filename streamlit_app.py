86cce74da1832707df86a4c599f1cf9fc068158f
AmulyaNidhi39 <84920720+AmulyaNidhi39@users.noreply.github.com>
2022-12-23 16:44:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,34 +55,69 @@ def check_password():
         return True
 
 if check_password():
-    server_url = 'https://prod-useast-a.online.tableau.com'
-    user = 'amulya.s.nidhi@kipi.bi'
-    password = 'Kipithon@123'
-    site = 'Site21'
-    tableau_auth = TSC.TableauAuth(username=user, password=password, site_id=site)
-    server = TSC.Server(server_url, use_server_version=True)
+    if username == user1:
+        server_url = 'https://prod-useast-a.online.tableau.com'
+        user = 'amulya.s.nidhi@kipi.bi'
+        password = 'Kipithon@123'
+        site = 'Site21'
+        tableau_auth = TSC.TableauAuth(username=user, password=password, site_id=site)
+        server = TSC.Server(server_url, use_server_version=True)
 
-    # Get various data.
-    # Explore the tableauserverclient library for more options.
-    # Uses st.experimental_memo to only rerun when the query changes or after 10 min.
-    @st.experimental_memo(ttl=1200)
-    def run_query(view_name):
-        with server.auth.sign_in(tableau_auth):
-            workbooks, pagination_item = server.workbooks.get()
-            for w in workbooks:
-                if w.name == 'ourworkbook':
-                    our_workbook = w
-                    break
-    # Get views for BHARAT_REFINERY_DASHBOARD_FINAL workbook.
-            server.workbooks.populate_views(our_workbook)
-            for v in our_workbook.views:
-                if view_name == v.name:
-                    our_view = v
-                    break
+        # Get various data.
+        # Explore the tableauserverclient library for more options.
+        # Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+        @st.experimental_memo(ttl=1200)
+        def run_query(view_name):
+            with server.auth.sign_in(tableau_auth):
+                workbooks, pagination_item = server.workbooks.get()
+                for w in workbooks:
+                    if w.name == 'ourworkbook':
+                        our_workbook = w
+                        break
+        # Get views for BHARAT_REFINERY_DASHBOARD_FINAL workbook.
+                server.workbooks.populate_views(our_workbook)
+                for v in our_workbook.views:
+                    if view_name == v.name:
+                        our_view = v
+                        break
 
-        #Get an image for the view.
-            server.views.populate_image(our_view)
-            view_image = our_view.image
-            return view_image
-    view_image = run_query('MyDash')
-    st.image(view_image, width=800)
+            #Get an image for the view.
+                server.views.populate_image(our_view)
+                view_image = our_view.image
+                return view_image
+        view_image = run_query('MyDash')
+        st.image(view_image, width=800)
+    elif username == user2:
+        server_url = 'https://prod-useast-a.online.tableau.com'
+        user = 'amulya.s.nidhi@kipi.bi'
+        password = 'Kipithon@123'
+        site = 'Site21'
+        tableau_auth = TSC.TableauAuth(username=user, password=password, site_id=site)
+        server = TSC.Server(server_url, use_server_version=True)
+
+        # Get various data.
+        # Explore the tableauserverclient library for more options.
+        # Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+        @st.experimental_memo(ttl=1200)
+        def run_query(view_name):
+            with server.auth.sign_in(tableau_auth):
+                workbooks, pagination_item = server.workbooks.get()
+                for w in workbooks:
+                    if w.name == 'ourworkbook':
+                        our_workbook = w
+                        break
+        # Get views for BHARAT_REFINERY_DASHBOARD_FINAL workbook.
+                server.workbooks.populate_views(our_workbook)
+                for v in our_workbook.views:
+                    if view_name == v.name:
+                        our_view = v
+                        break
+
+            #Get an image for the view.
+                server.views.populate_image(our_view)
+                view_image = our_view.image
+                return view_image
+        view_image = run_query('newdash')
+        st.image(view_image, width=800)
+    else:
+        st.print("zz")
