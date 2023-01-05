6088091f7e1f6a1fe69c7b0027f3488d0dbe709e
AmulyaNidhi39 <84920720+AmulyaNidhi39@users.noreply.github.com>
2023-01-05 17:54:06 +0530

    Update FINAL_APP.py

--- FINAL_APP.py
+++ FINAL_APP.py
@@ -328,7 +328,7 @@ elif choose == 'TABLEAU':
                 with server.auth.sign_in(tableau_auth):
                     workbooks, pagination_item = server.workbooks.get()
                     for w in workbooks:
-                        if w.name == 'Craig':
+                        if w.name == 'cj':
                             our_workbook = w
                             break
             # Get views for BHARAT_REFINERY_DASHBOARD_FINAL workbook.
@@ -342,7 +342,7 @@ elif choose == 'TABLEAU':
                     server.views.populate_image(our_view)
                     view_image = our_view.image
                     return view_image
-            view_image = run_query('craig')
+            view_image = run_query('Craig Dashboard')
             st.image(view_image, width=800)
         elif st.session_state["username"] == 'user2':
             server_url = 'https://prod-apnortheast-a.online.tableau.com'
@@ -360,7 +360,7 @@ elif choose == 'TABLEAU':
                 with server.auth.sign_in(tableau_auth):
                     workbooks, pagination_item = server.workbooks.get()
                     for w in workbooks:
-                        if w.name == 'Jhony':
+                        if w.name == 'cj':
                             our_workbook = w
                             break
             # Get views for BHARAT_REFINERY_DASHBOARD_FINAL workbook.
