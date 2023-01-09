6be2068f573bf88b7de0c851e9adc392f4b71495
AmulyaNidhi39 <84920720+AmulyaNidhi39@users.noreply.github.com>
2023-01-09 12:29:26 +0530

    Update FINAL_APP.py

--- FINAL_APP.py
+++ FINAL_APP.py
@@ -271,7 +271,7 @@ elif choose == 'TABLEAU':
                 with server.auth.sign_in(tableau_auth):
                     workbooks, pagination_item = server.workbooks.get()
                     for w in workbooks:
-                        if w.name == 'myworkbook1':
+                        if w.name == 'myworkbook12':
                             our_workbook = w
                             break
             # Get views for BHARAT_REFINERY_DASHBOARD_FINAL workbook.
